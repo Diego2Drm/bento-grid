c1fed6d9816cb65df32e367105ea66558b039574
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-10-18 21:41:48 -0600

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,10 @@ function App() {
   };
   const gridSquare = {
     hidden: { opacity: 0, y: 100 },
-    show: { opacity: 1, y: 0},
+    show: { opacity: 1, y: 0 },
   };
   return (
+    <>
       <motion.section
         variants={gridContainerVariants}
         initial="hidden"
@@ -33,13 +34,27 @@ function App() {
       >
         <FristComponent variants={gridSquare} />
         <SecondComponent variants={gridSquare} />
-        <ThirdComponent variants={gridSquare}/>
-        <FourthComponent variants={gridSquare}/>
-        <FifthComponent variants={gridSquare}/>
-        <SixthComponet variants={gridSquare}/>
-        <SeventhComponent variants={gridSquare}/>
-        <EighthComponent variants={gridSquare}/>
+        <ThirdComponent variants={gridSquare} />
+        <FourthComponent variants={gridSquare} />
+        <FifthComponent variants={gridSquare} />
+        <SixthComponet variants={gridSquare} />
+        <SeventhComponent variants={gridSquare} />
+        <EighthComponent variants={gridSquare} />
       </motion.section>
+
+      <motion.footer
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { delay: 3, duration: 2 } }}
+        className={style.footer}
+      >
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          {" "}
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Diego Ramírez</a>
+      </motion.footer>
+    </>
   );
 }
 
